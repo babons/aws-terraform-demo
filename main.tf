466376daa4b51c945b004ef98012962d1439c298
babons <sebastianbabon213@gmail.com>
2024-06-04 17:30:44 -0400

    created vpc and subnets with internet routing

--- main.tf
+++ main.tf
@@ -1,23 +1,57 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags   = {
+    Terraform = "true"
+    Environment = "dev"
   }
-
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-east-1"
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "ami-00beae93a2d981137"
-  instance_type = "t2.micro"
+resource "aws_subnet" "public" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    name        = "Public Subnet"
+    terraform   = "true"
+    environment = "dev"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    name        = "Private Subnet"
+    terraform   = "true"
+    environment = "dev"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    name       = "main-igw"
+    terraform  = "true"
+    environemt = "dev"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name        = "pubic-rt"
+    terraform   = "true"
+    environment = "dev"
   }
 }
