9e7dafe6ac91bddbd220ec587c62bb1ef0c72b53
babons <sebastianbabon213@gmail.com>
2024-06-04 20:46:35 -0400

    created bucket resource with versioning and server-side encryption

--- main.tf
+++ main.tf
@@ -1,57 +1,18 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_s3_bucket" "demo" {
+  bucket = "demo-bucket"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+  }
   
-  tags   = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
   tags = {
-    name        = "Public Subnet"
-    terraform   = "true"
-    environment = "dev"
-  }
-}
-
-resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    name        = "Private Subnet"
-    terraform   = "true"
-    environment = "dev"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    name       = "main-igw"
-    terraform  = "true"
-    environemt = "dev"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name        = "pubic-rt"
-    terraform   = "true"
-    environment = "dev"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
