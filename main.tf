17587acfff2de6c7aca717d49b04343b8d738885
babons <sebastianbabon213@gmail.com>
2024-06-04 21:11:08 -0400

    added public access block resource

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "aws_s3_bucket" "demo" {
-  bucket = "demo-bucket"
+  bucket = "babon-demo-bucket"
+  
   versioning {
     enabled = true
   }
+  
   server_side_encryption_configuration {
     rule {
         apply_server_side_encryption_by_default {
@@ -15,4 +17,13 @@ resource "aws_s3_bucket" "demo" {
     Name        = "My bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "demo" {
+  bucket = aws_s3_bucket.demo.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "aws_region" {
   description = "AWS region to launch servers."
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
