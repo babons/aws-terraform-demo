c4353cb0b376ca140f7ba9320f51c9ed33383458
babons <sebastianbabon213@gmail.com>
2024-06-09 11:33:30 -0400

    Added security group configurations

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+# --------- VPC Config w/ Subnets, Interney Gateway, and Routes ---------
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
@@ -60,16 +62,76 @@ resource "aws_route_table" "rt-main" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw-main.id
+    gateway_id = aws_internet_gateway.gw.id
   }
 }
 
-resource "aws_route_table_association" "rt-pub" {
-  subnet_id = aws_subnet.pub1.id
-  route_table_id = aws_route_table.rt-main
+resource "aws_route_table_association" "rt-pub1" {
+  subnet_id      = aws_subnet.pub1.id
+  route_table_id = aws_route_table.rt-main.id
 }
 
-resource "aws_route_table_association" "rt-pub" {
-  subnet_id = aws_subnet.pub2
-  route_table_id = aws_route_table.rt-main
+resource "aws_route_table_association" "rt-pub2" {
+  subnet_id      = aws_subnet.pub2.id
+  route_table_id = aws_route_table.rt-main.id
+}
+
+# --------- ALB Security Group ---------
+
+resource "aws_security_group" "alb_allow_http" {
+  name        = "alb_allow_http"
+  description = "Allow HTTP for all inbound and outbound traffic through ALB."
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "Allow HTTP ALB"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_allow_http" {
+  security_group_id = aws_security_group.alb_allow_http.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_allow_http" {
+  security_group_id = aws_security_group.alb_allow_http.id
+  cidr_ipv4         = aws_vpc.main.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_allow_http" {
+  security_group_id = aws_security_group.alb_allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "tcp"
+}
+
+# --------- EC2-ALB Security Group ---------
+
+resource "aws_security_group" "ec2-allow-alb" {
+  name        = "ec2-allow-alb"
+  description = "Allow traffic between EC2 instances and ALB."
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 80
+    to_port         = 80
+    security_groups = [aws_security_group.alb_allow_http.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow EC2 to ALB"
+  }
 }
