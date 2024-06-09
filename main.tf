80201fcf1cd5a5d7cdc085fadfb988431bbe65ea
babons <sebastianbabon213@gmail.com>
2024-06-09 16:19:44 -0400

    Added ALB configuration

--- main.tf
+++ main.tf
@@ -163,5 +163,33 @@ resource "aws_autoscaling_group" "app_deployment" {
   launch_template {
     id = aws_launch_template.ec2-web.id
   } 
+}
 
+# --------- ALB Configuration ---------
+
+resource "aws_lb" "app_alb" {
+  name               = "app-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_allow_http.id]
+  subnets            = [aws_subnet.pub1.id, aws_subnet.pub2.id]
+}
+
+resource "aws_lb_target_group" "alb_pub_tg" {
+  name        = "alb-pub-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "instance"
+}
+
+resource "aws_lb_listener" "ab_pub_listener" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_pub_tg.arn
+  }
 }
