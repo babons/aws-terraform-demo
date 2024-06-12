23d8aff8c8f1f540932607eea7da26d185797b1f
Sebastian Babon <sbabon@cert.org>
2024-06-12 09:35:44 -0400

    Added 404 page

--- main.tf
+++ main.tf
@@ -58,6 +58,18 @@ resource "aws_s3_object" "object" {
   etag = filemd5("./index.html")
 }
 
+resource "aws_s3_object" "object2" {
+  bucket = aws_s3_bucket.demo.id
+  key    = "404.html"
+  source = "./404.html"
+  content_type = "text/html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("./index.html")
+}
+
 resource "aws_s3_bucket_policy" "demo_policy" {
   bucket = aws_s3_bucket.demo.id
 
